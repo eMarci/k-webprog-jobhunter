842314dbf889c87c403ac71cd79199cd369af05e
Marton Endredi <endredimarci@gmail.com>
2024-05-30 15:24:33 +0200

    API slice átnevezése

--- client/src/store/jobhunterApi.js
+++ client/src/store/jobhunterApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = 'http://localhost:3030';
-export const jobhunterApiSlice = createApi({
+export const jobhunterApi = createApi({
   reducerPath: 'jobhunterApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
