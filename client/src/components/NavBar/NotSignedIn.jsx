506b33dc5bacee1ea1ae7acb57bef8fca66d836b
Marton Endredi <endredimarci@gmail.com>
2024-05-31 14:01:26 +0200

    Nem bejelentkezett navbar navigáció

--- client/src/components/NavBar/NotSignedIn.jsx
+++ client/src/components/NavBar/NotSignedIn.jsx
@@ -1,10 +1,13 @@
 import NavButton from "./NavButton/NavButton.jsx";
+import { useNavigate } from "react-router-dom";
 
 const NotSignedIn = () => {
+  const nav = useNavigate();
+
   return <>
     <NavButton>Főoldal</NavButton>
-    <NavButton>Regisztráció</NavButton>
-    <NavButton>Bejelentkezés</NavButton>
+    <NavButton onClick={() => nav('/register')}>Regisztráció</NavButton>
+    <NavButton onClick={() => nav('/login')}>Bejelentkezés</NavButton>
   </>;
 };
 
