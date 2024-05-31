5ba06f1ec3ce02de42c64b2817a4797d18b33e1d
Marton Endredi <endredimarci@gmail.com>
2024-05-31 12:30:28 +0200

    Navbarok közötti választás implementálva

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,16 @@
-import NavButton from "./NavButton/NavButton.jsx";
 import Employee from "./Employee.jsx";
 import Employer from "./Employer.jsx";
 import NotSignedIn from "./NotSignedIn.jsx";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const auth = useSelector(state => state.auth.auth);
+
   return <>
     <div className="navbar bg-primary">
       <div className="navbar-start"></div>
       <div className="navbar-center gap-x-10">
-        { true ? (true ? <Employee/> : <Employer/>) : <NotSignedIn/> }
+        { auth?.accessToken ? (auth?.user?.role === 'jobseeker' ? <Employee/> : <Employer/>) : <NotSignedIn/> }
       </div>
       <div className="navbar-end"></div>
     </div>
