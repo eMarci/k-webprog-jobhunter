6da26248e9680a0fdc7bf1c16846b3d0240adcf9
Marton Endredi <endredimarci@gmail.com>
2024-06-07 22:12:31 +0200

    Router útvonalak hozzáadása

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import Login from "./components/Authenticate/Login.jsx";
 import Register from "./components/Authenticate/Register.jsx";
 import Home from "./components/Home/Home.jsx";
 import Account from "./components/Account/Account.jsx";
+import JobInfo from "./components/JobInfo/JobInfo.jsx";
 
 const App = () => {
   return <>
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<UseAuthentication><Home/></UseAuthentication>}/>
-          <Route path="/home" element={<UseAuthentication><Home/></UseAuthentication>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="/account" element={<UseAuthentication><Account/></UseAuthentication>}/>
+          <Route path="/job/:id" element={<JobInfo/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="*" element={<Login/>}/>
