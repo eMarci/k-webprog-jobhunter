d963ed5de0075eedaab374f39dadebc4aae7b736
Marton Endredi <endredimarci@gmail.com>
2024-06-07 22:06:43 +0200

    Nem bejelentkezett navbar javítások

--- client/src/components/NavBar/NotSignedIn.jsx
+++ client/src/components/NavBar/NotSignedIn.jsx
@@ -1,13 +1,10 @@
 import NavButton from "./NavButton/NavButton.jsx";
-import { useNavigate } from "react-router-dom";
 
 const NotSignedIn = () => {
-  const nav = useNavigate();
-
   return <>
-    <NavButton>Főoldal</NavButton>
-    <NavButton onClick={() => nav('/register')}>Regisztráció</NavButton>
-    <NavButton onClick={() => nav('/login')}>Bejelentkezés</NavButton>
+    <NavButton to="/home">Főoldal</NavButton>
+    <NavButton to="/register">Regisztráció</NavButton>
+    <NavButton to="/login">Bejelentkezés</NavButton>
   </>;
 };
 
